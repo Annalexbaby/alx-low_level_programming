d18fb972d53ea64219926a3fe586773ef23457f0
Annalexbaby <tobiadesiyan007@gmail.com>
2023-08-17 06:44:32 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = a;
+    int largest;
 
-	if (largest < b)
-	{
-	largest = b;
-	if (largest < c)
-		largest = c;
-	}
-	return (largest);
+    if (a > b || b > c)
+    {
+        largest = a;
+    }
+    else if (b > a || a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
